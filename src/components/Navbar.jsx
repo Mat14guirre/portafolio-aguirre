2dff1c1ecba529a2c6e450ba50ce19bd35a872a5
Matias Aguirre <matias.aguirre269@gmail.com>
2025-03-04 14:18:54 -0300

    aplicando colores y estilos a la pagina

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // Estado para manejar la visibilidad del menú
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Cambia el estado de isOpen al hacer clic
+  };
+
   return (
     <nav className="navbar">
-      <ul>
-        <li><a href="#home">Inicio</a></li>
-        <li><a href="#about">Sobre mi</a></li>
-        <li><a href="#projects">Proyectos</a></li>
-        <li><a href="#contact">Contacto</a></li>
-      </ul>
+      <div className="navbar-container">
+        {/* Ícono del menú hamburguesa */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <h1 className='name-navbar'>Matias Ezequiel Aguirre</h1>
+        <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+          <li><a href="#home">Inicio</a></li>
+          <li><a href="#about">Sobre mi</a></li>
+          <li><a href="#projects">Proyectos</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const HomePage = () => {
   return (
     <section id="home" className="home-page">
-      <h1>BIENVENIDOS!</h1>
-      <p> Desarrollador full stack</p>
+      <h1>Bienvenidos a mi portafolio</h1>
+      <p>Desarrollador full stack</p>
     </section>
   );
 }
